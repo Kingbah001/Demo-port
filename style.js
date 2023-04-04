34b72225affcc35a8a9fe90e0835da616bcf7950
Kingbah001 <114989677+Kingbah001@users.noreply.github.com>
2023-04-04 03:24:25 -0700

    A little correction

--- style.js
+++ style.js
@@ -10,7 +10,7 @@ var hamburgerdiv = document.querySelector(".hamburger");
 var mobileLinks = document.querySelector(".mobile-link-holder")
 var mybackdrop = document.querySelector(".backdrop")
 
-function dosomething (){
+function dosomething() {
   hamburgerdiv.classList.toggle("showburger")
   mobileLinks.classList.toggle("show-mobile-link-holder")
   mybackdrop.classList.toggle("show-backdrop")
